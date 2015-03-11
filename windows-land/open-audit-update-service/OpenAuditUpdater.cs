e0cd2af7c44e8df29613107dd1ae066e4266c817
Pedro Augusto das Chagas <chagaspac@gmail.com>
2015-03-11 17:19:51 -0300

    Fixing bugs

--- windows-land/open-audit-update-service/OpenAuditUpdater.cs
+++ windows-land/open-audit-update-service/OpenAuditUpdater.cs
@@ -27,104 +27,130 @@ namespace open_audit_update_service
 
         static void Main(string[] args)
         {
-            util.writeToLogFile("Starting Update");
-            #region Hide Console Window In Order To Update Quietly
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
-            #endregion
-
-            #region If the computer has internet working
-            util.writeToLogFile("Testing Internet Connection");
-            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() &&
-                new System.Net.NetworkInformation.Ping().Send("www.eigmercados.com.br").Status == IPStatus.Success)
+            try
             {
-                util.writeToLogFile("There is Internet Connection");
-                #region Download Files
-                try
-                {
 
-                    util.writeToLogFile("Creating temporary directory");
-                    if (Directory.Exists(util.getPfPath() + "\\temp"))
+                util.writeToLogFile("Starting Update");
+                #region Hide Console Window In Order To Update Quietly
+                var handle = GetConsoleWindow();
+                ShowWindow(handle, SW_HIDE);
+                #endregion
+
+                #region If the computer has internet working
+                util.writeToLogFile("Testing Internet Connection");
+                if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() &&
+                    new System.Net.NetworkInformation.Ping().Send("www.eigmercados.com.br").Status == IPStatus.Success)
+                {
+                    util.writeToLogFile("There is Internet Connection");
+
+                    #region Check If Service are Stopped, if not stop them
+                    util.writeToLogFile("Stopping services");
+                    stopService("open-audit-service");
+                    stopService("open-audit-check-service");
+                    #endregion
+
+
+                    #region Download Files
+                    try
+                    {
+
+                        util.writeToLogFile("Creating temporary directory");
+                        if (Directory.Exists(util.getPfPath() + "\\temp"))
+                            Directory.Delete(util.getPfPath() + "\\temp", true);
+                        Directory.CreateDirectory(util.getPfPath() + "\\temp");
+
+
+                        util.writeToLogFile("Downloading files...");
+                        String mainPath = "https://eigmercados.com.br/open_audit/update/";
+                        WebClient webClient = new WebClient();
+                        webClient.DownloadFile(mainPath + "open-audit-service.exe", util.getPfPath() + "\\temp\\open-audit-service.exe");
+                        webClient.DownloadFile(mainPath + "open-audit-config.exe", util.getPfPath() + "\\temp\\open-audit-config.exe");
+                        webClient.DownloadFile(mainPath + "open-audit-check-service.exe", util.getPfPath() + "\\temp\\open-audit-check-service.exe");
+                        webClient.DownloadFile(mainPath + "open-audit.conf", util.getPfPath() + "\\temp\\open-audit.conf");
+                        webClient.DownloadFile(mainPath + "open-audit-lib.dll", util.getPfPath() + "\\temp\\open-audit-lib.dll");
+                    }
+                    catch (Exception e)
+                    {
+                        util.writeToLogFile(e.Message);
+                        util.writeToLogFile(e.StackTrace);
+                        Thread.Sleep(30000);
+                        Main(null);
+                    }
+                    #endregion
+
+                    util.writeToLogFile("Updating");
+
+                    //BKP Obj
+                    ConfigObj config = util.readConfig();
+                    
+                    int error_count = 0;
+                    while (!replaceFiles() && error_count < 5)
+                    {
+                        error_count++;
+                        Thread.Sleep(3000);
+                    }
+
+                    if (error_count < 5)
+                    {
+                        ConfigObj newConfig = util.readConfig();
+                        newConfig.strId = config.strId;
+                        util.writeConfig(newConfig, util.getConfPath(Constants.CONF_PATH));
+
+
+                        #region Clean Temp Directory
+
+                        util.writeToLogFile("Cleaning Temporary Files");
                         Directory.Delete(util.getPfPath() + "\\temp", true);
-                    Directory.CreateDirectory(util.getPfPath() + "\\temp");
 
+                        #endregion
 
-                    util.writeToLogFile("Downloading files...");
-                    String mainPath = "https://eigmercados.com.br/open_audit/update/";
-                    WebClient webClient = new WebClient();
-                    webClient.DownloadFile(mainPath + "open-audit-service.exe", util.getPfPath() + "\\temp\\open-audit-service.exe");
-                    webClient.DownloadFile(mainPath + "open-audit-config.exe", util.getPfPath() + "\\temp\\open-audit-config.exe");
-                    webClient.DownloadFile(mainPath + "open-audit-check-service.exe", util.getPfPath() + "\\temp\\open-audit-check-service.exe");
-                    webClient.DownloadFile(mainPath + "open-audit.conf", util.getPfPath() + "\\temp\\open-audit.conf");
-                    webClient.DownloadFile(mainPath + "open-audit-lib.dll", util.getPfPath() + "\\temp\\open-audit-lib.dll");
+                        #region Start Services
+                        util.writeToLogFile("Starting services again");
+                        startService("open-audit-service");
+                        startService("open-audit-check-service");
+                        util.writeToLogFile("DONE!");
+                        #endregion
+
+                    }
+                    else
+                    {
+                        util.writeToLogFile("Não foi possível atualizar os arquivos baixados, tentando novamente...");
+                        Thread.Sleep(5000);
+                        Main(null);
+                    }
                 }
-                catch (Exception)
+                else
                 {
+                    util.writeToLogFile("There isn't internet connection, trying again in 30s");
                     Thread.Sleep(30000);
                     Main(null);
                 }
                 #endregion
-
-
-                #region Check If Service are Stopped, if not stop them
-                util.writeToLogFile("Stopping services");
-                stopService("open-audit-service");
-                stopService("open-audit-check-service");
-                #endregion
-
-
-                #region Copy New Files
-                util.writeToLogFile("Updating");
-
-                //BKP Obj
-                ConfigObj config = util.readConfig();
-
-                File.Copy(util.getPfPath() + "\\temp\\open-audit-service.exe", util.getPfPath() + "\\open-audit-service.exe", true);
-                File.Copy(util.getPfPath() + "\\temp\\open-audit-config.exe", util.getPfPath() + "\\open-audit-config.exe", true);
-                File.Copy(util.getPfPath() + "\\temp\\open-audit-check-service.exe", util.getPfPath() + "\\open-audit-check-service.exe", true);
-                File.Copy(util.getPfPath() + "\\temp\\open-audit.conf", util.getPfPath() + "\\conf\\open-audit.conf", true);
-                File.Copy(util.getPfPath() + "\\temp\\open-audit-lib.dll", util.getPfPath() + "\\open-audit-lib.dll", true);
-
-                ConfigObj newConfig = util.readConfig();
-                newConfig.strId = config.strId;
-                util.writeConfig(newConfig, util.getConfPath(Constants.CONF_PATH));
-
-                #endregion
-
-                #region Clean Temp Directory
-
-                util.writeToLogFile("Cleaning Temporary Files");
-                Directory.Delete(util.getPfPath() + "\\temp", true);
-
-                #endregion
-
-                #region Start Services
-                util.writeToLogFile("Starting services again");
-                startService("open-audit-service");
-                startService("open-audit-check-service");
-                util.writeToLogFile("DONE!");
-                #endregion
             }
-            else
+            catch (Exception e)
             {
-                util.writeToLogFile("There isn't internet connection, trying again in 30s");
-                Thread.Sleep(30000);
-                Main(null);
+                util.writeToLogFile(e.Message);
+                util.writeToLogFile(e.StackTrace);
             }
-            #endregion
+
         }
 
         private static void stopService(string serviceName)
         {
             try
             {
+                util.writeToLogFile("Trying stop" + serviceName);
                 ServiceController sc = new ServiceController(serviceName);
                 if (sc.Status != ServiceControllerStatus.Stopped)
                     sc.Stop();
+                Thread.Sleep(3000);
+                if (sc.Status != ServiceControllerStatus.Stopped)
+                    stopService(serviceName);
             }
             catch (Exception e)
             {
                 util.writeToLogFile(e.Message);
+                util.writeToLogFile(e.StackTrace);
             }
         }
 
@@ -135,11 +161,34 @@ namespace open_audit_update_service
                 ServiceController sc = new ServiceController(serviceName);
                 if (sc.Status != ServiceControllerStatus.StartPending)
                     sc.Start();
+                if (sc.Status != ServiceControllerStatus.StartPending)
+                    startService(serviceName);
             }
             catch (Exception e)
             {
                 util.writeToLogFile(e.Message);
+                util.writeToLogFile(e.StackTrace);
             }
+        }
+
+        private static bool replaceFiles()
+        {
+            bool ret = true;
+            try
+            {
+                File.Copy(util.getPfPath() + "\\temp\\open-audit-service.exe", util.getPfPath() + "\\open-audit-service.exe", true);
+                File.Copy(util.getPfPath() + "\\temp\\open-audit-config.exe", util.getPfPath() + "\\open-audit-config.exe", true);
+                File.Copy(util.getPfPath() + "\\temp\\open-audit-check-service.exe", util.getPfPath() + "\\open-audit-check-service.exe", true);
+                File.Copy(util.getPfPath() + "\\temp\\open-audit.conf", util.getPfPath() + "\\conf\\open-audit.conf", true);
+                File.Copy(util.getPfPath() + "\\temp\\open-audit-lib.dll", util.getPfPath() + "\\open-audit-lib.dll", true);
+            }
+            catch (Exception e)
+            {
+                util.writeToLogFile(e.Message);
+                util.writeToLogFile(e.StackTrace);
+                ret = false;
+            }
+            return ret;
         }
     }
 }
