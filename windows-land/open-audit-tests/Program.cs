2a49f4d55865c65ea521a403e3c272d9da44ee60
lucasassislar <lucasassis@outlook.com>
2015-05-20 14:35:12 -0300

    Created test project

---
+++ windows-land/open-audit-tests/Program.cs
@@ -0,0 +1,18 @@
+﻿using open_audit_lib.threads;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_audit_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceThreadImpl t = new ServiceThreadImpl();
+            t.runDownloadTrafficSensor();
+        }
+    }
+}
